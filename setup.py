0fbc96e4f8dc63b53ff36d6721254857cd4b3794
zblurx <68540460+zblurx@users.noreply.github.com>
2022-10-19 15:44:25 +0200

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="acltoolkit",
-    version="0.0.1",
+    version="0.1.0",
     author="zblurx",
     url="https://github.com/zblurx/acltoolkit",
     long_description="README.md",
